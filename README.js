11d58d4fb6213d2aff97de8bb5fd09321bd72483
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-05-11 10:55:06 -0300

    Update README.js

--- README.js
+++ README.js
@@ -3,7 +3,7 @@ const GleisonPires = {
   Name: "Gleison A. Pires da Silva",
   About: "Developer passionate about Cloud Infrastructure and Machine Learning, working at Compass.uol",
   Age: 19,
-  iStudio: Cloud & DevSecOps
+  iStudio: Cloud & DevSecOps,
   Skills: [ AWS, Oracle Cloud Infrastructure, Azure, GCP, Terraform, C, CI/CD, SaaS, PaaS],
   Contact: [ Instagram: '@Glleison_p'; Linkedin: 'https://www.linkedin.com/in/gleisonpires/' ]
 };
